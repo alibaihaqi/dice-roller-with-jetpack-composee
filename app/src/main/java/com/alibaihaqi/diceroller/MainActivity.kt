d239c2f44d3f6aa12a771795d5aff20f653cd2a5
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-10-28 23:36:03 +0700

    feat: add button string roll

--- app/src/main/java/com/alibaihaqi/diceroller/MainActivity.kt
+++ app/src/main/java/com/alibaihaqi/diceroller/MainActivity.kt
@@ -3,14 +3,17 @@ package com.alibaihaqi.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.alibaihaqi.diceroller.ui.theme.DiceRollerTheme
 
@@ -32,12 +35,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DiceWithButtonAndImage(
-    modifier: Modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center)
 ) {
-
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(R.string.roll))
+        }
+    }
 }
 
 @Preview(showBackground = true)
